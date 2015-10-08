0455a80819f1fe342b8806aaf0fb3aaa74bd19fc
DarkWanderer <elquentaro@gmail.com>
2015-10-08 23:06:05 +0300

    Added Token class, TokenType enum

---
+++ LuaParser/Language/Token.cs
@@ -0,0 +1,31 @@
+﻿namespace DW.Lua.Token
+{
+    public class Token
+    {
+        public Token(string value, int startPosition, int endPosition, TokenType tokenType)
+        {
+            Value = value;
+            StartPosition = startPosition;
+            EndPosition = endPosition;
+            TokenType = tokenType;
+        }
+
+        public string Value { get; }
+
+        public int StartPosition { get; }
+
+        public int EndPosition { get; }
+
+        public TokenType TokenType { get; }
+    }
+
+    public enum TokenType
+    {
+        StringConstant,
+        NumericConstant,
+        BooleanOperator,
+        ArithmeticOperator,
+        Keyword,
+        Identifier
+    }
+}
