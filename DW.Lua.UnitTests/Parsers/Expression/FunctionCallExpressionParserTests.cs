e70c52e234dc5d9343c78f5c3f18eb76c6657c4a
DarkWanderer <elquentaro@gmail.com>
2016-01-07 15:47:58 +0300

    Added new unit tests for expression parsers

---
+++ DW.Lua.UnitTests/Parsers/Expression/FunctionCallExpressionParserTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DW.Lua.UnitTests.Parsers.Expression
+{
+    public static class FunctionCallExpressionParserTests
+    {
+        [Test]
+        public static void ShouldParseNoArgumentsCall()
+        {
+            SyntaxParser.Parse("a = b()");
+            Assert.Inconclusive("Need to check return value");
+        }
+
+        [Test]
+        public static void ShouldParseMultipleArgumentsCall()
+        {
+            SyntaxParser.Parse("a = b(c,d,e)");
+            Assert.Inconclusive("Need to check return value");
+        }
+    }
+}
---
+++ DW.Lua.UnitTests/Parsers/Expression/StringConstantExpressionParserTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DW.Lua.UnitTests.Parsers.Expression
+{
+    public static class StringConstantExpressionParserTests
+    {
+        [Test]
+        public static void ShouldParseStringAssignment()
+        {
+            SyntaxParser.Parse("a = \"TEST\"");
+        }
+
+        [Test]
+        public static void ShouldParseMultilineStringAssignment()
+        {
+            SyntaxParser.Parse("a = [[TEST1\nTEST2]]");
+        }
+    }
+}
